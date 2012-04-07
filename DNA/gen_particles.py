6a4aa892e82ec14ba4254ec73bc3e8a5565c1da3
Justin MacCallum <justin.maccallum@me.com>
2012-04-07 01:35:49 -0400

    Messing around with particles.

---
+++ DNA/gen_particles.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from random import uniform, gauss
+
+of = open('particles.rib', 'w')
+
+for i in range(500000):
+
+    x = uniform(-3.8, 1.5)
+    y = uniform(-.8, 2.5)
+    z = uniform(-7, 1)
+
+    x_rot = gauss(-1, 1)
+    y_rot = gauss(-1, 1)
+    z_rot = gauss(-1, 1)
+    angle = uniform(0, 360)
+    scale = gauss(0.003, 0.0015)
+
+    print >>of, 'TransformBegin'
+    print >>of, '   Translate %f %f %f' % (x, y, z)
+    print >>of, '   Rotate %f %f %f %f' % (angle, x_rot, y_rot, z_rot)
+    print >>of, '   Scale %f %f %f' % (scale, scale, scale)
+    print >>of, '   Polygon "P" [-0.5 0.5 0  0.5 0.5 0  0.5 -0.5 0  -0.5 -0.5 0]'
+    print >>of, 'TransformEnd'
+
+
